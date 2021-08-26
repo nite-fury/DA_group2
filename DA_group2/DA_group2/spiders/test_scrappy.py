ab5f553d10df6c4d884fe90ec08f0adc39f9e250
lennon <lennon_su@outlook.com>
2021-08-26 12:57:13 +0800

    complete all tests ready for final submission

--- DA_group2/DA_group2/spiders/test_scrappy.py
+++ DA_group2/DA_group2/spiders/test_scrappy.py
@@ -1,27 +1,15 @@
 import unittest
-import Browser
-import requests
 import os
-
-class TestBrowser(unittest.TestCase):
-    def test_startreq(self):
-        rh = requests.get(Script.url)
-
-print("User Agent:")
-print(Script.rh.text)
-
-headers = {
-    'User-Agent': 'Mobile'
-}
-
-print("User Agent:")
-print(Script.rh.text)
-
-class TestAllProgram(unittest.TestCase):
+class Testrecon(unittest.TestCase):
     def test(self):
         os.system("C:\\Users\\lenno\\Desktop\\DA_group2\\DA_group2\\DA_group2\\spiders\\Browser.py")
-        os.system("C:\\Users\\lenno\\Desktop\\DA_group2\\DA_group2\\DA_group2\\spiders\\scrapimage.py")
+
+class Testresponse(unittest.TestCase):
+    def test(self):
         os.system("C:\\Users\\lenno\\Desktop\\DA_group2\\DA_group2\\DA_group2\\spiders\\scrapindex.py")
 
+class Testimage(unittest.TestCase):
+    def test(self):
+        os.system("C:\\Users\\lenno\\Desktop\\DA_group2\\DA_group2\\DA_group2\\spiders\\scrapimage.py")
 if __name__ == '__main__':
     unittest.main()
